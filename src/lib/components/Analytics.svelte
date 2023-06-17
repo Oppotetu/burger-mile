4e2c71148666faa905dff4e948cc65ac37cec5a3
simonhaughom <simon.haughom@gmail.com>
2023-06-17 13:59:02 +0200

    changes

--- src/lib/components/Analytics.svelte
+++ src/lib/components/Analytics.svelte
@@ -3,7 +3,7 @@
 
 	$: {
 		if (typeof gtag !== 'undefined') {
-			gtag('config', process.env.MEASUREMENT_ID, {
+			gtag('config', 'G-E1S1TKK3YG', {
 				page_title: document.title,
 				page_path: $page.url.pathname
 			})
@@ -12,7 +12,7 @@
 </script>
 
 <svelte:head>
-	<script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.MEASUREMENT_ID}`}>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-E1S1TKK3YG">
 	</script>
 	<script>
 		window.dataLayer = window.dataLayer || []
@@ -22,6 +22,6 @@
 		}
 
 		gtag('js', new Date())
-		gtag('config', process.env.MEASUREMENT_ID)
+		gtag('config', 'G-E1S1TKK3YG')
 	</script>
 </svelte:head>
