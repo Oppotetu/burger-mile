c501db967e535917a4e67198aa156401f07e9b3e
simonhaughom <simon.haughom@gmail.com>
2023-06-20 21:23:46 +0200

    score badge

--- src/lib/components/BurgerCard.svelte
+++ src/lib/components/BurgerCard.svelte
@@ -20,13 +20,13 @@
 
 <Geolocation getPosition bind:coords />
 
-<div class="card variant-filled-secondary z-[9999] p-4" data-popup="popupHover">
+<!-- <div class="card variant-filled-secondary z-[9999] p-4" data-popup="popupHover">
 	<p>Hover Content</p>
 	<div class="arrow variant-filled-secondary" />
-</div>
+</div> -->
 
-<a class="card card-hover w-80 overflow-hidden md:w-60" href={`/${joint.slug.current}`}>
-	<header class="card-header p-0">
+<a class="card card-hover w-80 md:w-60" href={`/${joint.slug.current}`}>
+	<header class="card-header relative p-0">
 		<img
 			class="h-56 w-full rounded-md bg-black/50 md:h-64"
 			src={$size.width <= 768
@@ -34,21 +34,27 @@
 				: urlFor(joint.image.asset._ref).width(240).height(256).url()}
 			alt={joint.slug ? joint.slug.current : 'burger-image'}
 		/>
+		{#if joint.average}
+			<span
+				class="badge-icon absolute -bottom-2 -left-3 h-12 w-12 border-2 bg-primary-500 border-surface-900-50-token"
+				>{joint.average}</span
+			>
+		{/if}
 	</header>
 	<section class="space-y-1 p-2 dark:bg-surface-800">
+		<div class="mt-2 flex place-items-center justify-between">
+			<span class="text-xl">
+				{joint.name}
+			</span>
+			{#if joint.price === 1 || joint.price === 2}
+				<span class="badge variant-ghost-success">$</span>
+			{:else if joint.price === 3 || joint.price === 4}
+				<span class="badge variant-ghost-success">$$</span>
+			{:else if joint.price === 5 || joint.price === 6}
+				<span class="badge variant-ghost-success">$$$</span>
+			{/if}
+		</div>
 		<ul class="list -space-y-3">
-			<li>
-				<span class="text-xl">
-					{joint.name}
-				</span>
-				{#if joint.price === 1 || joint.price === 2}
-					<span class="badge variant-ghost-success">$</span>
-				{:else if joint.price === 3 || joint.price === 4}
-					<span class="badge variant-ghost-success">$$</span>
-				{:else if joint.price === 5 || joint.price === 6}
-					<span class="badge variant-ghost-success">$$$</span>
-				{/if}
-			</li>
 			<li>
 				<p>🍔</p>
 				<ProgressBar
@@ -79,7 +85,7 @@
 					track="bg-surface-300 dark:bg-surface-500"
 				/>
 			</li>
-			<li>
+			<!-- <li>
 				<p use:popup={popupHover}>🎲</p>
 				<ProgressBar
 					label="Score"
@@ -88,50 +94,8 @@
 					meter="bg-primary-500"
 					track="bg-surface-300 dark:bg-surface-500"
 				/>
-			</li>
+			</li> -->
 		</ul>
-		<!-- 
-		<div class="flex place-items-center [&>*]:pointer-events-none">
-			<p class="pr-2" use:popup={popupHover}>🎲</p>
-			<ProgressBar
-				label="Score"
-				value={joint.average}
-				max={6}
-				meter="bg-primary-500"
-				track="bg-surface-300 dark:bg-surface-500"
-			/>
-		</div>
-		<div class="flex place-items-center">
-			<p class="pr-2">🍔</p>
-			<ProgressBar
-				label="Score"
-				value={joint.food}
-				max={6}
-				meter="bg-secondary-500"
-				track="bg-surface-300 dark:bg-surface-500"
-			/>
-		</div>
-		<div class="flex place-items-center">
-			<p class="pr-2">🎉</p>
-			<ProgressBar
-				label="Score"
-				value={joint.atmosphere}
-				max={6}
-				meter="bg-warning-300"
-				track="bg-surface-300 dark:bg-surface-500"
-			/>
-		</div>
-		<div class="flex place-items-center">
-			<p class="pr-2">😎</p>
-			<ProgressBar
-				label="Score"
-				value={joint.itFactor}
-				max={6}
-				meter="bg-tertiary-500"
-				track="bg-surface-300 dark:bg-surface-500"
-			/>
-		</div>
-		<div /> -->
 	</section>
 	<footer class="card-footer flex justify-center">
 		<span class="badge variant-ghost-secondary mr-2">{joint.city}</span>
@@ -140,6 +104,3 @@
 		{/if}
 	</footer>
 </a>
-<!-- {#each { length: 6 - joint.food } as _}
-<span><img src={grayBurger} alt="grayburger" /></span>
-{/each} -->
--- src/routes/[resto]/+page.svelte
+++ src/routes/[resto]/+page.svelte
@@ -44,7 +44,7 @@
 			{resto.review}
 		</p>
 
-		{#if resto.pros}
+		<!-- {#if resto.pros}
 			<div class="card grid grid-cols-1 gap-1 md:mx-[12%] md:!grid-cols-3">
 				<div class="p-2 md:p-6">
 					<h4 class="w-full pb-2">Pros 👍</h4>
@@ -69,7 +69,7 @@
 					{/if}
 				</span>
 			</div>
-		{/if}
+		{/if} -->
 
 		<ul class="card list -space-y-3 md:mx-[12%]">
 			<li>
@@ -102,7 +102,7 @@
 					track="bg-surface-300 dark:bg-surface-500"
 				/>
 			</li>
-			<br />
+			<!-- <br />
 			<li>
 				<p>🎲</p>
 				<ProgressBar
@@ -112,8 +112,27 @@
 					meter="bg-primary-500"
 					track="bg-surface-300 dark:bg-surface-500"
 				/>
-			</li>
+			</li> -->
 		</ul>
+
+		<div class="card grid grid-cols-1 gap-1 md:mx-[12%] md:!grid-cols-2">
+      <span class="p-2 md:p-6">
+        <h4 class="w-full pb-2">Price 💰</h4>
+        {#if resto.price === 1 || resto.price === 2}
+          <span class="badge variant-ghost-success h-12 w-12">$</span>
+        {:else if resto.price === 3 || resto.price === 4}
+          <span class="badge variant-ghost-success h-12 w-12">$$</span>
+        {:else if resto.price === 5 || resto.price === 6}
+          <span class="badge variant-ghost-success h-12 w-12">$$$</span>
+        {/if}
+      </span>
+			<span class="flex flex-col place-items-center p-2 md:p-6">
+				<h4 class="w-full pb-2">Score 🎲</h4>
+				<span class="badge-icon h-12 w-12 border-2 bg-primary-500 border-surface-900-50-token"
+					>{resto.average}</span
+				>
+			</span>
+		</div>
 
 		<div class="card p-6 text-center md:col-start-3 md:row-start-4 md:mx-[12%]">
 			<p>Reviewed in {resto.year}</p>
@@ -122,124 +141,22 @@
 			</p>
 		</div>
 	</div>
-
-	<!-- <p
-		class="p-6 text-left md:col-start-1 md:col-end-3 md:row-start-2 md:row-end-5 md:px-[12%] md:pt-2"
-	>
-		{resto.review}
-	</p>
-
-	<div class="md:col-start-3 md:row-start-2 md:-ml-4 md:pr-10">
-		<h3>Pros 👍</h3>
-		<span class="chip variant-ghost-success">fresh</span>
-		<span class="chip variant-ghost-success">frosh</span>
-		<h4>Cons 👎</h4>
-		<span class="chip variant-ghost-error">soggy</span>
-		<span class="chip variant-ghost-error">overcooked</span>
-	</div>
-
-	<div class="p-6 md:col-start-3 md:row-start-3 md:-ml-4 md:pr-10">
-		<div class="flex place-items-center">
-			<p class="mr-2 [&>*]:pointer-events-none">🎲</p>
-			<ProgressBar
-				label="Score"
-				value={resto.average}
-				max={6}
-				meter="bg-primary-500"
-				track="bg-surface-300 dark:bg-surface-500"
-			/>
-		</div>
-		<div class="flex place-items-center">
-			<p class="mr-2">🍔</p>
-			<ProgressBar
-				label="Score"
-				value={resto.food}
-				max={6}
-				meter="bg-secondary-500"
-				track="bg-surface-300 dark:bg-surface-500"
-			/>
-		</div>
-		<div class="flex place-items-center">
-			<p class="mr-2">💰</p>
-			<ProgressBar
-				label="Score"
-				value={resto.price}
-				max={6}
-				meter="bg-success-400"
-				track="bg-surface-300 dark:bg-surface-500"
-			/>
-		</div>
-		<div class="flex place-items-center">
-			<p class="mr-2">🎉</p>
-			<ProgressBar
-				label="Score"
-				value={resto.atmosphere}
-				max={6}
-				meter="bg-warning-300"
-				track="bg-surface-300 dark:bg-surface-500"
-			/>
-		</div>
-		<div class="flex place-items-center">
-			<p class="mr-2">😎</p>
-			<ProgressBar
-				label="Score"
-				value={resto.itFactor}
-				max={6}
-				meter="bg-tertiary-500"
-				track="bg-surface-300 dark:bg-surface-500"
-			/>
-		</div>
-	</div>
-	<div class="text-center md:col-start-3 md:row-start-4 md:-ml-4 md:pr-10">
-		<p>Reviewed in {resto.year}</p>
-		<p class="p-2 pb-5">
-			Check out {resto.name} in the <a href={`map?setView=${resto.slug.current}`}>map</a>!
-		</p>
-	</div> -->
 </div>
-
-<!-- <div class="text-center md:col-start-3 md:row-start-4 md:-ml-4 md:pr-10">
-  <p>{resto.name} is located at {resto.adress}</p>
-  <p>Check out {resto.name} in the <a href={`map?setView=${resto.slug.current}`}>map</a>!</p>
-</div> -->
-
-<!-- <div class="container mx-auto flex flex-col justify-center text-center md:p-10">
-	<h1 class="p-2 md:p-8">{resto.name}</h1>
-	<h4>Reviewed in {resto.year}</h4>
-	<hr class="py-6 md:py-8" />
-
-	<div class="md:pb-8">
-		<p class="pb-10 md:float-left md:w-1/2 md:pb-8 md:pr-12 lg:px-20 lg:pb-12">{resto.review}</p>
-		<div class="pb-4 md:float-right md:w-1/2">
-			<div class="lg:flex lg:justify-center">
-				<img
-					class="rounded-md bg-black/50 md:max-w-sm lg:max-w-md"
-					src={resto.image && urlFor(resto.image.asset._ref).url()}
-					alt={resto.slug.current}
-				/>
-			</div>
-		</div>
-		<div class="py-6 md:w-1/2 md:pr-12">
-			<p>Food: {resto.food}</p>
-			<p>Price: {resto.price}</p>
-			<p>Atmosphere: {resto.atmosphere}</p>
-			<p>It-Factor: {resto.itFactor}</p>
-			<br />
-			<p>Total score: {resto.average}</p>
-		</div>
-	</div>
-
-	<hr class="py-6" />
-
-	<div>
-		<p>{resto.name} is located at <br />{resto.adress}</p>
-		<br />
-		<p>Check out {resto.name} in the <a href={`map?setView=${resto.slug.current}`}>map</a>!</p>
-	</div>
-</div> -->
 
 <style>
 	@media (min-width: 768px) {
+		.grid-template {
+			grid-template-rows: 12rem 1fr;
+			/* grid-template-rows: 6.5rem repeat(3, minmax(0, 1fr)); */
+		}
+	}
+	@media (min-width: 1024px) {
+		.grid-template {
+			grid-template-rows: 8.5rem 1fr;
+			/* grid-template-rows: 6.5rem repeat(3, minmax(0, 1fr)); */
+		}
+	}
+	@media (min-width: 1281px) {
 		.grid-template {
 			grid-template-rows: 6.5rem 1fr;
 			/* grid-template-rows: 6.5rem repeat(3, minmax(0, 1fr)); */
